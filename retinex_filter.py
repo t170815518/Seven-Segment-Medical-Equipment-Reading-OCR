f08b5fa58f0e28c3fd72e37e4a2c2f0e8c545590
t170815518 <christang_1023@outlook.com>
2021-02-03 03:49:04 +0800

    Adapt correct fast_bilateral_filter

--- retinex_filter.py
+++ retinex_filter.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2 as cv
+import math
 from math import floor, inf
-from scipy import ndimage
+import scipy
+from scipy import ndimage, signal
 from scipy.interpolate import interpn
 import matplotlib.pyplot as plt
 
@@ -16,7 +18,7 @@ def retinex_filter(img, sampling_spatial, sampling_range, gamma, sigma_range=0.2
     reflection = illumination
 
     # find illumination by filtering with envelope mode
-    illumination = fast_bilateral_filter(illumination, sigma_spatial, sigma_range, sampling_spatial, sampling_range)
+    illumination = bilateral_approximation(illumination, illumination, sigma_spatial, sigma_range, sampling_spatial, sampling_range)
 
     # if is_show_plot:
     #     cv.imshow("illumination", reflection - illumination)
@@ -24,10 +26,10 @@ def retinex_filter(img, sampling_spatial, sampling_range, gamma, sigma_range=0.2
 
     # find reflection by filtering with regular mode at this point reflection stores original image
     reflection = (reflection - illumination)
-    reflection = fast_bilateral_filter(reflection, sigma_spatial, sigma_range, sampling_spatial, sampling_range)
+    reflection = bilateral_approximation(reflection, reflection, sigma_spatial, sigma_range, sampling_spatial, sampling_range)
 
     if is_show_plot:
-        plt.imshow(np.exp(illumination), cmap='gray', clim=(0, 1))
+        plt.imshow(np.exp(illumination), cmap='gray')
         plt.show()
     # apply gamma correction to illumination
     illumination = 1 / gamma * illumination
@@ -35,99 +37,88 @@ def retinex_filter(img, sampling_spatial, sampling_range, gamma, sigma_range=0.2
     return np.exp(reflection + illumination)
 
 
-def fast_bilateral_filter(img, sigmaSpatial, sigmaRange, samplingSpatial, samplingRange):
-    """ Uses convolution with normal gaussian kernel """
-    img_size = img.shape
+def bilateral_approximation(data, edge, sigmaS, sigmaR, samplingS=None, samplingR=None, edgeMin=None, edgeMax=None):
+    """ Ref: https://gist.github.com/jackdoerner/b81ad881c4064470d3c0 """
+    # This function implements Durand and Dorsey's Signal Processing Bilateral Filter Approximation (2006)
+    # It is derived from Jiawen Chen's matlab implementation
+    # The original papers and matlab code are available at http://people.csail.mit.edu/sparis/bf/
 
-    max_val = np.max(img.flatten())
-    min_val = np.min(img.flatten())
-    span_val = max_val - min_val
+    inputHeight = data.shape[0]
+    inputWidth = data.shape[1]
+    samplingS = sigmaS if (samplingS is None) else samplingS
+    samplingR = sigmaR if (samplingR is None) else samplingR
+    edgeMax = np.amax(edge) if (edgeMax is None) else edgeMax
+    edgeMin = np.amin(edge) if (edgeMin is None) else edgeMin
+    edgeDelta = edgeMax - edgeMin
+    derivedSigmaS = sigmaS / samplingS;
+    derivedSigmaR = sigmaR / samplingR;
 
-    derivedSigmaSpatial = sigmaSpatial / samplingSpatial
-    derivedSigmaRange = sigmaRange / samplingRange
+    paddingXY = math.floor(2 * derivedSigmaS) + 1
+    paddingZ = math.floor(2 * derivedSigmaR) + 1
 
-    # to avoid checking points on borders in Gaussian convolution, create a 0 band width
-    # and height 'padding' around the grid
-    paddingXY = floor(2 * derivedSigmaSpatial) + 1
-    paddingZ = floor(2 * derivedSigmaRange) + 1
+    # allocate 3D grid
+    downsampledWidth = math.floor((inputWidth - 1) / samplingS) + 1 + 2 * paddingXY
+    downsampledHeight = math.floor((inputHeight - 1) / samplingS) + 1 + 2 * paddingXY
+    downsampledDepth = math.floor(edgeDelta / samplingR) + 1 + 2 * paddingZ
 
-    # create grid
-    downsampledWidth = floor((img_size[1] - 1) / samplingSpatial) + 1 + 2 * paddingXY
-    downsampledHeight = floor((img_size[0] - 1) / samplingSpatial) + 1 + 2 * paddingXY
-    downsampledDepth = int(floor(span_val / samplingRange) + 1 + 2 * paddingZ)
+    gridData = np.zeros((downsampledHeight, downsampledWidth, downsampledDepth))
+    gridWeights = np.zeros((downsampledHeight, downsampledWidth, downsampledDepth))
 
-    grid_wi = np.zeros((downsampledHeight, downsampledWidth, downsampledDepth))
-    grid_w = np.zeros((downsampledHeight, downsampledWidth, downsampledDepth))
+    # compute downsampled indices
+    (jj, ii) = np.meshgrid(range(inputWidth), range(inputHeight))
 
-    # down-sampling
-    for i in range(img_size[0]):
-        for j in range(img_size[1]):
-            pixel_val = img[i, j]
+    di = np.around(ii / samplingS) + paddingXY
+    dj = np.around(jj / samplingS) + paddingXY
+    dz = np.around((edge - edgeMin) / samplingR) + paddingZ
 
-            if not np.isnan(pixel_val):
-                x = round(i / samplingSpatial) + paddingXY + 1
-                y = round(j / samplingSpatial) + paddingXY + 1
-                ksi = int(round((pixel_val - min_val / samplingRange) + paddingZ + 1))
+    # perform scatter (there's probably a faster way than this)
+    # normally would do downsampledWeights( di, dj, dk ) = 1, but we have to
+    # perform a summation to do box downsampling
+    for k in range(dz.size):
 
-            grid_wi[x, y, ksi] = grid_wi[x, y, ksi] + pixel_val
-            grid_w[x, y, ksi] = grid_w[x, y, ksi] + 1
+        dataZ = data.flat[k]
+        if (not math.isnan(dataZ)):
+            dik = int(di.flat[k])
+            djk = int(dj.flat[k])
+            dzk = int(dz.flat[k])
 
-    # create Guassian Kernal
-    kernelWidth = 2 * derivedSigmaSpatial + 1
+            gridData[dik, djk, dzk] += dataZ
+            gridWeights[dik, djk, dzk] += 1
+
+    # make gaussian kernel
+    kernelWidth = 2 * derivedSigmaS + 1
     kernelHeight = kernelWidth
-    kernelDepth = 2 * derivedSigmaRange + 1
+    kernelDepth = 2 * derivedSigmaR + 1
 
-    grid_wi_b = Gaussian_conv_3d(kernelWidth, kernelHeight, kernelDepth, derivedSigmaSpatial, derivedSigmaRange, grid_wi)
-    grid_w_b = Gaussian_conv_3d(kernelWidth, kernelHeight, kernelDepth, derivedSigmaSpatial, derivedSigmaRange, grid_w)
+    halfKernelWidth = math.floor(kernelWidth / 2)
+    halfKernelHeight = math.floor(kernelHeight / 2)
+    halfKernelDepth = math.floor(kernelDepth / 2)
 
-    # divide wi_b / w_b
-    normalizedBlurredGrid = np.divide(grid_wi_b, grid_w_b, out=np.zeros_like(grid_wi_b), where=grid_w_b!=0)
+    (gridX, gridY, gridZ) = np.meshgrid(range(int(kernelWidth)), range(int(kernelHeight)), range(int(kernelDepth)))
+    gridX -= halfKernelWidth
+    gridY -= halfKernelHeight
+    gridZ -= halfKernelDepth
+    gridRSquared = ((gridX * gridX + gridY * gridY) / (derivedSigmaS * derivedSigmaS)) + (
+                (gridZ * gridZ) / (derivedSigmaR * derivedSigmaR))
+    kernel = np.exp(-0.5 * gridRSquared)
 
-    # up-sample
-    jj, ii = np.meshgrid(range(img_size[1]), range(img_size[0]))
+    # convolve
+    blurredGridData = signal.fftconvolve(gridData, kernel, mode='same')
+    blurredGridWeights = signal.fftconvolve(gridWeights, kernel, mode='same')
 
-    di = (ii / samplingSpatial) + paddingXY + 1
-    dj = (jj / samplingSpatial) + paddingXY + 1
-    dz = (img - min_val) / samplingRange + paddingZ + 1
+    # divide
+    blurredGridWeights = np.where(blurredGridWeights == 0, -2,
+                                     blurredGridWeights)  # avoid divide by 0, won't read there anyway
+    normalizedBlurredGrid = blurredGridData / blurredGridWeights;
+    normalizedBlurredGrid = np.where(blurredGridWeights < -1, 0,
+                                        normalizedBlurredGrid)  # put 0s where it's undefined
 
-    # adopt code from matlab: interpn
-    pixelGrid = [np.arange(x) for x in normalizedBlurredGrid.shape]
-    interpPoints = (np.array([di.flatten(), dj.flatten(), dz.flatten()])).T
+    # upsample
+    (jj, ii) = np.meshgrid(range(inputWidth), range(inputHeight))
+    # no rounding
+    di = (ii / samplingS) + paddingXY
+    dj = (jj / samplingS) + paddingXY
+    dz = (edge - edgeMin) / samplingR + paddingZ
 
-    result = interpn(pixelGrid, normalizedBlurredGrid, interpPoints)
-    result = np.reshape(result, di.shape)
-    return result
-
-
-def Gaussian_conv_3d(kernelWidth, kernelHeight, kernelDepth, derivedSigmaSpatial, derivedSigmaRange, grid):
-    kernalX = gauss2D((kernelWidth, 1), derivedSigmaSpatial)
-    kernalY = gauss2D((1, kernelHeight), derivedSigmaSpatial)
-    kernalZ = gauss2D((kernelDepth, 1), derivedSigmaRange)
-    # expand kernal to be in the same shape as X
-    kernalX = np.expand_dims(kernalX, axis=0)
-    kernalY = np.expand_dims(kernalY, axis=0)
-    kernalZ = np.expand_dims(kernalZ, axis=0)
-
-    k = np.zeros((1, 1, int(round(kernelDepth))))
-    k[:] = np.reshape(kernalZ, (1, -1))
-
-    data2 = grid
-    data2 = ndimage.convolve(data2, kernalX)  # replace matplotlib.convn
-    data2 = ndimage.convolve(data2, kernalY)
-    data2 = ndimage.convolve(data2, k)
-    return data2
-
-
-def gauss2D(shape=(3,3),sigma=0.5):
-    """
-    2D gaussian mask - should give the same result as MATLAB's fspecial('gaussian',[shape],[sigma])
-    Ref: https://stackoverflow.com/a/17201686/11180198
-    """
-    m,n = [(ss-1.)/2. for ss in shape]
-    y,x = np.ogrid[-m:m+1,-n:n+1]
-    h = np.exp(-(x*x + y*y) / (2.*sigma*sigma) )
-    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
-    sumh = h.sum()
-    if sumh != 0:
-        h /= sumh
-    return h
+    return scipy.interpolate.interpn((range(normalizedBlurredGrid.shape[0]), range(normalizedBlurredGrid.shape[1]),
+                                      range(normalizedBlurredGrid.shape[2])), normalizedBlurredGrid, (di, dj, dz))
