48320e537fe40bf185fc6ab69dac3f3c658deeaa
t170815518 <christang_1023@outlook.com>
2021-02-13 22:39:33 +0800

    Completes blob_extraction_MSER

--- blob_extraction.py
+++ blob_extraction.py
@@ -3,34 +3,39 @@ from math import sqrt, floor
 import numpy as np
 import cv2 as cv
 from retinex_filter import retinex_filter
+from utils import get_bbox_overlap_ratio
 
 
-def blob_extraction_CC(img, hue):
-    mser = cv.MSER_create()
+def blob_extraction_CC(img, hue=None, threshold=0.0001):
+    """
+
+    :param img: binarized
+    :param hue: hue channel of the img
+    :param threshold:
+    :return:
+    """
+    img = img.astype(np.uint8)  # convert float array to 8-bit
+    num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(img, threshold, 8, cv.CV_32S)
+    pass
+
+
+def blob_extraction_MSER(img, config):
+    mser = cv.MSER_create(_delta=int(config.delta * 100), _max_variation=config.max_are_variant)
     regions, bboxes = mser.detectRegions(img.astype(np.uint8))
-    hue_values = []
-    for region in regions:  # regions: list of point sets
-        hue_values.append(hue[region])
-
-
-def blob_extraction_MSER(img, up, config):
-    sigmaSpatial = config.sigmaSpatial
-    sigmaRange = config.sigmaRange
-    samplingSpatial = sigmaSpatial
-    samplingRange = config.sigmaRange
-    gamma = config.gamma
-    delta = config.delta
-    T = config.T
-
-    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    value = hsv_img[2]
-    hue = hsv_img[0]
-
-    v_retinex = retinex_filter(value, sigmaSpatial, sigmaRange, samplingSpatial, samplingRange, gamma, 0)
-
-    mser = cv.MSER(_area_threshold=delta*100, _max_variation=T)
-    points, bboxes = mser.detectRegions(v_retinex)
-    img_mask = np.zeros_like(img)
-    img_mask[points] = 1
-
-    # Only take one region from all overlapping regions
+    areas = bboxes[:, 2] * bboxes[:, 3]
+    # get rid of intersecting regions
+    keeped = []
+    all_bbox = bboxes.T
+    for i, (region, bbox) in enumerate(zip(regions, bboxes)):
+        overlap_ratios = get_bbox_overlap_ratio(bbox, all_bbox)
+        overlap_ratios[i] = 0
+        try:
+            indices = np.where(overlap_ratios > 0.8)
+            indices = np.argmax(areas[indices])
+            keeped.append(indices)
+        except ValueError:  # attempt to get argmax of an empty sequence
+            keeped.append(i)
+    # remove redundant element from keeped
+    keeped = set(keeped)
+    keeped = list(keeped)
+    return (np.array(regions)[keeped], bboxes[keeped])
