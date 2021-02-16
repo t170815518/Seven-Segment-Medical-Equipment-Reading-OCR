7f6dee2db01e22211dd54577b094ab923a1bceb0
t170815518 <christang_1023@outlook.com>
2021-02-17 01:23:01 +0800

    Store the old preprocessing

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import cv2 as cv
 import numpy as np
 import random as rng
 from math import floor
-from skimage.filters import threshold_sauvola
+from skimage.filters import threshold_sauvola, threshold_niblack
 from preprocess import preprocess, Sauvola_binarize
 from blob_extraction import blob_extraction_CC, blob_extraction_MSER
 import matplotlib.pyplot as plt
+from homofilt import HomomorphicFilter
 
 
 if __name__ == '__main__':
     config = SimpleNamespace()
-    config.alpha = 0.02
-    config.k = 0.04
+    config.alpha = 0.01
+    config.k = 0.2
     config.delta = 0.02
     config.max_are_variant = 0.3
 
 
-    img_path = "img/actual_user_input.PNG"
+    img_path = "img/bp_test.jpg"
     img = cv.imread(img_path, cv.IMREAD_COLOR)
     try:
         img = cv.resize(img, (500, 700))
@@ -34,23 +35,27 @@ if __name__ == '__main__':
     plt.show()
     # blob extraction
     window_size = floor(v_img.shape[0] * config.alpha)
-
-    try:
-        v_binarised = threshold_sauvola(v_img, window_size, config.k)
-    except ValueError:  # when odd is not permitted
-        window_size -= 1
-        v_binarised = threshold_sauvola(v_img, window_size, config.k)
-
-    plt.imshow(v_binarised, cmap="gray")
+    v_binary = v_img
+    # v_binary = v_binary.astype(np.uint8)
+    # thresh = cv.adaptiveThreshold(v_binary, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    kernel1 = cv.getStructuringElement(cv.MORPH_RECT, (4, 30))
+    # dilation = cv.dilate(v_binary, kernel1, iterations=1)
+    # erosion = cv.erode(dilation, kernel1, iterations=3)
+    # thresh = cv.morphologyEx(v_binary, cv.MORPH_OPEN, kernel1)
+    closing = cv.morphologyEx(v_binary, cv.MORPH_OPEN, kernel1)
+    plt.imshow(closing, cmap="gray")
+    plt.show()
+    v_binary = closing
+    # blur = cv.GaussianBlur(v_binary, (5, 5), 0)
+    v_binary = v_binary.astype(np.uint8)
+    # v_binary = cv.adaptiveThreshold(v_binary, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
+    #                      cv.THRESH_BINARY, 11, 2)
+    _, v_binary = cv.threshold(v_binary, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    plt.imshow(v_binary, cmap="gray")
     plt.show()
 
-    v_binarised = v_binarised.astype(np.uint8)
-    # thresh = cv.adaptiveThreshold(v_binarised, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    blur = cv.GaussianBlur(v_binarised, (5, 5), 0)
-    ret, thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-    plt.imshow(thresh, cmap="gray")
-    plt.show()
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    v_binary = v_binary.astype(np.uint8)
+    contours, hierarchy = cv.findContours(v_binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     contours_poly = [None] * len(contours)
     boundRect = [None] * len(contours)
@@ -61,10 +66,10 @@ if __name__ == '__main__':
     # Draw polygonal contour + bonding rects + circles
     for i in range(len(contours)):
         color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-        cv.drawContours(v_binarised, contours_poly, i, color)
-        cv.rectangle(v_binarised, (int(boundRect[i][0]), int(boundRect[i][1])), (int(boundRect[i][0]+boundRect[i][2]),
+        cv.drawContours(v_binary, contours_poly, i, color)
+        cv.rectangle(v_binary, (int(boundRect[i][0]), int(boundRect[i][1])), (int(boundRect[i][0]+boundRect[i][2]),
                                                                                  int(boundRect[i][1]+boundRect[i][3])),
                      color, 2)
 
-    cv.imshow("contours", v_binarised)
+    cv.imshow("contours", v_binary)
     cv.waitKey(0)
