c13fe6e66c363ea0cb089a441d57d596db0dcbff
t170815518 <christang_1023@outlook.com>
2021-02-17 03:28:57 +0800

    Complete Bg

--- main.py
+++ main.py
@@ -9,67 +9,65 @@ from preprocess import preprocess, Sauvola_binarize
 from blob_extraction import blob_extraction_CC, blob_extraction_MSER
 import matplotlib.pyplot as plt
 from homofilt import HomomorphicFilter
+import seaborn as sb
+from sklearn.preprocessing import normalize
+from collections import defaultdict
+from pytesseract import image_to_string
+import os
 
 
 if __name__ == '__main__':
-    config = SimpleNamespace()
-    config.alpha = 0.01
-    config.k = 0.2
-    config.delta = 0.02
-    config.max_are_variant = 0.3
+    img_path = "img/actual_user_input.PNG"
+    BIN_Y = 70
+    BIN_X = 50
+    DSIZE = (500, 700)
+    os.environ["TESSDATA_PREFIX"] = os.path.join(os.getcwd(), "letsgodigital")
 
-
-    img_path = "img/bp_test.jpg"
     img = cv.imread(img_path, cv.IMREAD_COLOR)
+    img = cv.resize(img, DSIZE)
+    img = cv.cvtColor(img, cv.COLOR_BGR2YCR_CB)
+    cb_img = img[:, :, 2]
+    # create a CLAHE object (Arguments are optional).
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cb_img = clahe.apply(cb_img)
+    blur = cv.GaussianBlur(cb_img, (5, 5), 0)
+    _, binary_img = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    plt.imshow(binary_img, cmap="gray")
+    plt.show()
+
+    kernal = cv.getStructuringElement(cv.MORPH_RECT, (90, 10))
+    dilation = cv.dilate(binary_img, kernal, iterations=1)
+    plt.imshow(dilation, cmap="gray")
+    plt.show()
+
+    contours, hierarchy = cv.findContours(dilation, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    show_img = cv.cvtColor(binary_img, cv.COLOR_GRAY2BGR)
+    boxes = []
+    for c in contours:
+        (x, y, w, h) = cv.boundingRect(c)
+        boxes.append((x, y, w, h))
+        cv.rectangle(show_img, (x, y), (x + w, y + h), (255, 255, 0), 4)
+
+    # cv.imshow("contours", show_img)
+    # cv.waitKey(0)
+    box_info = {}
+    for i, box in enumerate(boxes):
+        x, y, w, h = box
+        crop_img = binary_img[y:y + h, x:x + w]
+
+        plt.imshow(crop_img, cmap="gray")
+        plt.show()
+        result = image_to_string(crop_img, lang="letsgodigital", config="--psm 13 -c tessedit_char_whitelist=.0123456789")
+        box_info[i] = {"area": w * h, "reading": result}
+    boxes_info = list(box_info.items())
+    box_info = sorted(boxes_info, key=lambda x: x[1]["area"], reverse=True)
+    is_valid_result = False
     try:
-        img = cv.resize(img, (500, 700))
-    except cv.error:  # when the img size < (500, 700)
-        img = cv.imread(img_path, cv.IMREAD_COLOR)
-
-    img_hue = cv.cvtColor(img, cv.COLOR_BGR2HSV_FULL)  # convert image to hsv
-    hue = img_hue[:, :, 0]
-    # image enhancement
-    v_img = preprocess(img_hue)
-
-    plt.imshow(v_img, cmap="gray")
-    plt.show()
-    # blob extraction
-    window_size = floor(v_img.shape[0] * config.alpha)
-    v_binary = v_img
-    # v_binary = v_binary.astype(np.uint8)
-    # thresh = cv.adaptiveThreshold(v_binary, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    kernel1 = cv.getStructuringElement(cv.MORPH_RECT, (4, 30))
-    # dilation = cv.dilate(v_binary, kernel1, iterations=1)
-    # erosion = cv.erode(dilation, kernel1, iterations=3)
-    # thresh = cv.morphologyEx(v_binary, cv.MORPH_OPEN, kernel1)
-    closing = cv.morphologyEx(v_binary, cv.MORPH_OPEN, kernel1)
-    plt.imshow(closing, cmap="gray")
-    plt.show()
-    v_binary = closing
-    # blur = cv.GaussianBlur(v_binary, (5, 5), 0)
-    v_binary = v_binary.astype(np.uint8)
-    # v_binary = cv.adaptiveThreshold(v_binary, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
-    #                      cv.THRESH_BINARY, 11, 2)
-    _, v_binary = cv.threshold(v_binary, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    plt.imshow(v_binary, cmap="gray")
-    plt.show()
-
-    v_binary = v_binary.astype(np.uint8)
-    contours, hierarchy = cv.findContours(v_binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    contours_poly = [None] * len(contours)
-    boundRect = [None] * len(contours)
-    for i, c in enumerate(contours):
-        contours_poly[i] = cv.approxPolyDP(c, 3, True)
-        boundRect[i] = cv.boundingRect(contours_poly[i])
-
-    # Draw polygonal contour + bonding rects + circles
-    for i in range(len(contours)):
-        color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-        cv.drawContours(v_binary, contours_poly, i, color)
-        cv.rectangle(v_binary, (int(boundRect[i][0]), int(boundRect[i][1])), (int(boundRect[i][0]+boundRect[i][2]),
-                                                                                 int(boundRect[i][1]+boundRect[i][3])),
-                     color, 2)
-
-    cv.imshow("contours", v_binary)
-    cv.waitKey(0)
+        while not is_valid_result:
+            item = box_info.pop(0)
+            reading = float(item[1]["reading"].strip())
+            if 0 < reading <= 25:
+                is_valid_result = True
+                print(reading)
+    except IndexError:
+        raise ValueError("No reading recognized.")
